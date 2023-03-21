1cd784dfd87163792d6d2cf7dbcaa6bba2e874a1
Lurian Ortiz <lurian502@gmail.com>
2023-03-21 16:41:14 -0300

    Primeiro commit

---
+++ src/components/Banner.js
@@ -0,0 +1,9 @@
+import imgBanner from '../../public/img/banner.png';
+
+export const Banner = () => {
+    return(
+        <>
+            <img src={imgBanner} alt='Imagem banner'/>
+        </>
+    );
+}
